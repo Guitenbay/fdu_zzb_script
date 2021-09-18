7c26553379fc8f5385a046db2515f861709e3423
Guitenbay <gtenbay@163.com>
2021-09-18 20:23:25 +0800

    更新 umi-request

--- api/index.js
+++ api/index.js
@@ -1,64 +1,32 @@
-const request = require('./request');
+const { extend } = require("umi-request");
 
-// export function getWXUserInfo() {
-//   return request('get', '/api/fudan/wx/login');
-// }
+const request = extend({
+  timeout: 60000, // 6s * 10
+});
 
-// export function getRankingList({ type }) {
-//   return request('get', '/rank', { type });
-// }
+const createHandleResponse = ({ retCode, retMsg, results }) => {
+  console.log(results);
+  if (retCode !== 1000) {
+    if (console) console.error(retMsg ?? `获取 ${url} 数据失败`);
+    return null;
+  } else {
+    return results ?? true;
+  }
+};
 
-// export function playGame({ openid, type }) {
-//   return request('post', '/play', { openid, type });
-// }
-
-// export function invite({ openid, code }) {
-//   return request('post', '/invite', { openid, code });
-// }
-
-// export function getQuestion(token, page = 1, limit = 20) {
-//   return request(
-//     'get',
-//     `/api/fudan/volunteer/exam/list?page=${page}&limit=${limit}`,
-//     {},
-//     token,
-//   );
-// }
-
-// export function getScore({ openid, questionId, choice, costTime }) {
-//   return request('post', '/score', { openid, questionId, choice, costTime });
-// }
-
-// export function getTeamInfo(page = 1, limit = 50) {
-//   return request('get', `/api/fudan/volunteer/team/list?page=${page}&limit=${limit}`);
-// }
-
-function saveUserInfo({ username, mobile, userId, teamId }) {
-  return request('post', '/api/fudan/volunteer/appuser/save', {
-    username,
-    mobile,
-    userId,
-    teamId,
-  });
+function getTWList(data, cookie) {
+  return request
+    .post("https://zhtj.youth.cn/v1/center/getorgtree", {
+      data,
+      requestType: "form",
+      headers: {
+        Cookie: cookie,
+      },
+    })
+    .then(createHandleResponse);
 }
 
-// export function saveAnswer({ userId, examAnswer, examId }, token) {
-//   return request(
-//     'post',
-//     '/api/fudan/volunteer/answer/save',
-//     [{ userId, examAnswer, examId }],
-//     token,
-//   );
-// }
-
-// export function personalRank(token) {
-//   return request('post', '/api/fudan/volunteer/answer/personRank', {}, token);
-// }
-
-// export function teamRank(token) {
-//   return request('post', '/api/fudan/volunteer/answer/teamRank', {}, token);
-// }
-
 module.exports = {
-  saveUserInfo,
+  getTWList,
+  getTZBList,
 };
