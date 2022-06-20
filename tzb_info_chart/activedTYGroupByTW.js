f7ff0081f5308329ad5e33d4723f5c6c8c7c2b0c
Hummel Mao <gtenbay@163.com>
2022-06-20 19:22:04 +0800

    fix: 对研究生团工委单独处理

--- tzb_info_chart/activedTYGroupByTW.js
+++ tzb_info_chart/activedTYGroupByTW.js
@@ -10,10 +10,17 @@ const data = require("./tzb_list.json");
 const twMap = new Map();
 
 for (const tzb of data) {
-  const key = tzb.团委名 ?? tzb.团总支名 ?? tzb.支部名;
-  if (!twMap.has(key)) {
-    twMap.set(key, { 非流动: 0, 流动: 0 });
+  let key = '<none>';
+  // 研究生团工委需要细分
+  if (tzb.团委名 === '上海市复旦大学研究生团工委') {
+    key = tzb.团总支名 ?? tzb.支部名;
+  } else {
+    key = tzb.团委名 ?? tzb.团总支名 ?? tzb.支部名;
   }
+  if (!twMap.has(key)) {
+    twMap.set(key, { 所属团委:'', 非流动: 0, 流动: 0 });
+  }
+  twMap.get(key).所属团委 = tzb.团委名;
   if (!tzb.支部名.includes("流动")) {
     twMap.get(key).非流动 += tzb.组织人数;
   } else {
@@ -25,6 +32,7 @@ const result = [];
 twMap.forEach((value, key) => {
   result.push({
     '团委名/团总支': key,
+    所属团委: value.所属团委,
     非流动团员人数: value.非流动,
     流动团员人数: value.流动,
   });
